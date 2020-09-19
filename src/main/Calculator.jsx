7b5680d7ecda61d01f2d169a11ac571f2e3e15df
Alex Pierini <alex_pierini@hotmail.com>
2020-09-18 22:39:40 -0400

    Implémentation de la logique #02

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -26,7 +26,23 @@ export default class Calculator extends Component {
   }
 
   setOperation(operation) {
-    console.log(operation);
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true });
+    } else {
+      const equals = operation === "=";
+      const currentOperation = this.state.operation;
+      const values = [...this.state.values];
+      values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`);
+      values[1] = 0;
+
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values,
+      });
+    }
   }
 
   addDigit(n) {
@@ -34,18 +50,18 @@ export default class Calculator extends Component {
       return;
     }
 
-    const clearDisplay = this.state.displayValue === "0" || this.state.clearDisplay;
+    const clearDisplay =
+      this.state.displayValue === "0" || this.state.clearDisplay;
     const currentValue = clearDisplay ? "" : this.state.displayValue;
     const displayValue = currentValue + n;
     this.setState({ displayValue, clearDisplay: false });
 
-    if (n !== '.') {
+    if (n !== ".") {
       const index = this.state.current;
       const newValue = parseFloat(displayValue);
       const values = [...this.state.values];
       values[index] = newValue;
       this.setState({ values });
-      console.log(values);
     }
   }
 
